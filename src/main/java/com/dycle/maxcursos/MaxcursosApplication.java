92416f9cffa88ed045d531d531c83ba7443672b1
delcidecol <delcidecol@gmail.com>
2026-08-17 08:27:50 -0300

    Project created

---
+++ src/main/java/com/dycle/maxcursos/MaxcursosApplication.java
@@ -0,0 +1,13 @@
+package com.dycle.maxcursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxcursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaxcursosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dycle/maxcursos/MaxcursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dycle.maxcursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxcursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
