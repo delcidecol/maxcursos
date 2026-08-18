6dd5460716b646f5e73c308e77a52a7b8949f317
delcidecol <delcidecol@gmail.com>
2026-08-18 11:11:31 -0300

    Service layer, component registration

---
+++ src/main/java/com/dycle/maxcursos/services/UserService.java
@@ -0,0 +1,27 @@
+package com.dycle.maxcursos.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.dycle.maxcursos.entities.User;
+import com.dycle.maxcursos.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	private final UserRepository repository;  // dependencia do userRepository
+
+	UserService(UserRepository repository) {
+		this.repository = repository;
+	}
+		
+	public List<User> findAll(){   //metodo para retornar todos os usuarios
+		return repository.findAll();
+	}
+	public User findById(Long id) {
+		 Optional<User> obj = repository.findById(id);
+		 return obj.get();
+	}
+}
