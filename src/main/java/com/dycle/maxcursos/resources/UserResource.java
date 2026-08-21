2c618f3c3e75dca85d150e39e0c6bf507f530080
delcidecol <delcidecol@gmail.com>
2026-08-21 14:03:57 -0300

    User insert

--- src/main/java/com/dycle/maxcursos/resources/UserResource.java
+++ src/main/java/com/dycle/maxcursos/resources/UserResource.java
@@ -1,12 +1,16 @@
 package com.dycle.maxcursos.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.dycle.maxcursos.entities.User;
 import com.dycle.maxcursos.services.UserService;
@@ -20,17 +24,25 @@ public class UserResource {
 	UserResource(UserService service) {
 		this.service = service;
 	}
-	
+
 	@GetMapping
-	public ResponseEntity<List <User>> findAll(){
+	public ResponseEntity<List<User>> findAll() {
 		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+
 }
