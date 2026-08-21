9edebb9d85ae2c38c1c63a42e4b5e674092772ce
delcidecol <delcidecol@gmail.com>
2026-08-21 10:51:30 -0300

    Subtotal & Total menthods

--- src/main/java/com/dycle/maxcursos/entities/OrderItem.java
+++ src/main/java/com/dycle/maxcursos/entities/OrderItem.java
@@ -63,6 +63,12 @@ public class OrderItem implements Serializable {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
+	
+	// implementacao do metodo em que vai devolver o subtotal
+	
+	public Double getSubTotal() {
+		return price * quantity;
+	}
 
 	@Override
 	public int hashCode() {
