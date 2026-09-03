0ba4f026547ee3c84fecbc6e131f214a18124a93
delcidecol <delcidecol@gmail.com>
2026-09-03 13:11:06 -0300

    Exception handling - update

--- src/main/java/com/dycle/maxcursos/services/UserService.java
+++ src/main/java/com/dycle/maxcursos/services/UserService.java
@@ -12,6 +12,8 @@ import com.dycle.maxcursos.repositories.UserRepository;
 import com.dycle.maxcursos.services.exceptions.DatabaseException;
 import com.dycle.maxcursos.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -45,9 +47,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
