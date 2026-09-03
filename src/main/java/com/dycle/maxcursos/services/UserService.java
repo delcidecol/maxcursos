2b25a9b173323085d4976a31bbfb7e86d391adf9
delcidecol <delcidecol@gmail.com>
2026-09-03 09:42:42 -0300

    Exception handling - findById

--- src/main/java/com/dycle/maxcursos/services/UserService.java
+++ src/main/java/com/dycle/maxcursos/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.dycle.maxcursos.entities.User;
 import com.dycle.maxcursos.repositories.UserRepository;
+import com.dycle.maxcursos.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 	}
 	public User findById(Long id) {
 		 Optional<User> obj = repository.findById(id);
-		 return obj.get();
+		 return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
